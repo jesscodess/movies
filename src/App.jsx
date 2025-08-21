83e04b41c931c70759b6213441b9d17ca912a3c7
Jéssica Martins <jesscodes@proton.me>
2025-08-20 21:25:53 -0300

    First day commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = () => {
+const Card = ({ title }) => {
   return (
-    <h2>Functional Arrow Component</h2>
+    <div className="card">
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  const [hasLiked, setHasLiked] = useState();
+  return ( 
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true} />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
+    </div>
   )
 }
 
