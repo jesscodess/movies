bb95c93aab16e24b6a06a9651761e506c796fa8d
Jéssica Martins <jesscodes@proton.me>
2025-08-22 20:17:00 -0300

    Worked on Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked]);
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount (count + 1)}>
+      <h2>{title} <br /> {count || null}</h2>
+      <button onClick={() => setHasLiked (!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
-  const [hasLiked, setHasLiked] = useState();
   return ( 
     <div className="card-container">
       <Card title="Star Wars" rating={5} isCool={true} />
